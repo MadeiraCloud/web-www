f775f1625fc02cecf64458a51b228710efeb7988
MorrisLiang <morris@mc2.io>
2013-09-27 18:50:56 +0800

    update service css

--- script/main.js
+++ script/main.js
@@ -1,19 +1,36 @@
-(function(){
+$(function(){
 
   /* Scroll Effect for services */
-  var resizeTO = null
+  var resizeTO       = null
+  var baseline       = 0;
+
+  var viewportTop    = 0;
+  var viewportBottom = 0;
+  var docHeight      = 0;
+  var docEl          = document.documentElement;
 
   function onWinResize() {
     if ( resizeTO ) {
       clearTimeout( resizeTO);
     }
-    resizeTO = setTimeout( onWinScroll, 200 );
+    resizeTO = setTimeout( doResize, 200 );
   }
+
+  function doResize() {
+    var $services = $("#mcServices");
+    baseline = $services.offset().top + $services.height() + 100 - docEl.clientHeight;
+  }
+  doResize();
 
   function onWinScroll() {
-
+    var scrollY = window.scrollTop || window.scrollY;
+    if ( scrollY >= baseline ) {
+      // Show tags
+      $("#mcServices").removeTag("hide");
+      $(window).off("scroll", onWinScroll).off("resize", onWinResize);
+    }
   }
-  $(window).on("scroll", onWinScroll).on("resize", onWinResize);
+  $(window).
 
 
-})();
+});
