920f64577c1476ffd6cf93e65a02b4e114d8c963
MorrisLiang <morris@mc2.io>
2013-09-27 18:22:38 +0800

    static supported services

--- script/main.js
+++ script/main.js
@@ -1,4 +1,19 @@
 (function(){
 
+  /* Scroll Effect for services */
+  var resizeTO = null
+
+  function onWinResize() {
+    if ( resizeTO ) {
+      clearTimeout( resizeTO);
+    }
+    resizeTO = setTimeout( onWinScroll, 200 );
+  }
+
+  function onWinScroll() {
+
+  }
+  $(window).on("scroll", onWinScroll).on("resize", onWinResize);
+
 
 })();
